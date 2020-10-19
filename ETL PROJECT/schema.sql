85cdeeabc383d9320295750ddb044a1aab67c08e
Jordan <jordan.villaloboshernandez@gmail.com>
2020-10-19 18:45:04 -0500

    First push

---
+++ ETL PROJECT/schema.sql
@@ -0,0 +1,14 @@
+create table elections_2012(
+	state text primary key,
+	Democrat int,
+	Republican int,
+	year int
+);
+
+create table elections_2016(
+	state text primary key,
+	year int,
+	Republican int,
+	Democrat int
+);
+
